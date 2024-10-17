2bf3d9e3a028ac811ad297a216b0ede25ff777a4
patrick-kuenig <kuenig.patrick@gmail.com>
2024-10-17 22:51:11 +0300

    homework_17_10_2024

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'task2',
-    # 'task4',
+    'task2',
+    'task4',
     'task5',
 ]
 
